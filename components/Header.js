7e8b87ca4e1c7094c5f0c156f6ff100b549b51e6
João Felipe P <jf.pelliccione@gmail.com>
2022-07-24 18:49:56 -0300

    Construindo o Header Mobile!

--- components/Header.js
+++ components/Header.js
@@ -3,15 +3,26 @@ import Image from 'next/image';
 import ngLogo from '../assets/logo_ng_cash.gif';
 import styles from '../styles/components/Header.module.css';
 
+const ngLogoStyle = {
+  animationDuration: '1s',
+  animationTimingFunction: 'ease-out',
+  animationDelay: '0s',
+  animationIterationCount: 1,
+  animationName: 'slideInFromLeft',
+};
+
 export default function Header() {
   return (
     <header className={ styles.mobileHeader }>
       <Image
+        className={ styles.ngLogo }
         src={ ngLogo }
         alt="Logo NG.CASH"
-        className="ngLogo"
+        width="95px"
+        height="95px"
+        style={ ngLogoStyle }
       />
-      <span>Botão</span>
+      <span className={ styles.jfBtn }>Botão</span>
     </header>
   );
 }
