f6c7aa625f280b04b170bd3048beafd250fdb3ef
João Felipe P <jf.pelliccione@gmail.com>
2022-07-28 15:48:17 -0300

    Melhorando Header!

--- components/desktop/DesktopNavbar.js
+++ components/desktop/DesktopNavbar.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import headerPaths from '../../data/headerPaths';
 import styles from '../../styles/components/desktop/DesktopNavbar.module.css';
 
 export default function DesktopNavbar() {
+  const router = useRouter();
+
   return (
     <nav className={ styles.desktopNavbar }>
       {headerPaths.map(({ name, passHrefProp, path }) => (
@@ -14,6 +17,11 @@ export default function DesktopNavbar() {
         >
           <button
             type="button"
+            style={ {
+              borderBottom: router.pathname === path ? 'solid 1px #7D2CFF' : 'none',
+              color: router.pathname === path ? '#7D2CFF' : '#FFFFFF',
+              transitionDuration: '0.8s',
+            } }
           >
             {name}
           </button>
