5633544e3b743d824f51ad108c9f45a06fbe55dd
João Felipe P <jf.pelliccione@gmail.com>
2022-07-28 18:30:22 -0300

    Construção de um componente IMG.

---
+++ components/Img.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Image from 'next/image';
+
+export default function Img({ imgClassName, imgSrc, imgAlt }) {
+  return (
+    <div className={ imgClassName }>
+      <Image
+        src={ imgSrc }
+        alt={ imgAlt }
+        layout="responsive"
+      />
+    </div>
+  );
+}
+
+Img.propTypes = {
+  imgClassName: PropTypes.string.isRequired,
+  imgSrc: PropTypes.objectOf(PropTypes.string).isRequired,
+  imgAlt: PropTypes.string.isRequired,
+};
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Image from 'next/image';
+// import Image from 'next/image';
+import Img from '../components/Img';
 import indexMob1stTitle from '../assets/mobile/index/index_mob_1st_title.svg';
 import styles from '../styles/pages/home.module.css';
 
@@ -7,13 +8,11 @@ export default function Home() {
   return (
     <main className={ styles.homePageMain }>
       <section className={ styles.homepageFirstSection }>
-        <div className={ styles.homepage1stTitle }>
-          <Image
-            src={ indexMob1stTitle }
-            alt="1° título da Home"
-            layout="responsive"
-          />
-        </div>
+        <Img
+          imgClassName={ styles.homepage1stTitle }
+          imgSrc={ indexMob1stTitle }
+          imgAlt="1° título da Home"
+        />
       </section>
       <section className={ styles.homepageSecondSection }>
         <h1>Seção 2</h1>
