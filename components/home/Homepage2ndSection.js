da769644941b0d3045f3b406c4bf989bfa08dfd1
João Felipe P <jf.pelliccione@gmail.com>
2022-07-30 15:17:28 -0300

    Salvando progresso!

--- components/home/Homepage2ndSection.js
+++ components/home/Homepage2ndSection.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ImgBtn from '../ImgBtn';
 import homeMobSteps from '../../assets/mobile/home/home_mob_steps.svg';
+import homeMob2ndTitle from '../../assets/mobile/home/home_mob_2nd_title.svg';
 import downloadAppBtn from '../../assets/download_app_btn.svg';
 import styles from '../../styles/pages/home.module.css';
 import Img from '../Img';
@@ -12,6 +13,11 @@ export default function Homepage2ndSection() {
         imgClassName={ styles.home2ndSectionStepsMob }
         imgSrc={ homeMobSteps }
         imgAlt="Comece já!"
+      />
+      <Img
+        imgClassName={ styles.home2ndSectionTitleMob }
+        imgSrc={ homeMob2ndTitle }
+        imgAlt="Você está pronto!"
       />
       <ImgBtn
         imgClassName={ styles.headerDownloadAppBtn }
