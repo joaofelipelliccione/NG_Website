bd58651f4901aac5ce234cfb82b3b404de2bc254
João Felipe P <jf.pelliccione@gmail.com>
2022-07-30 12:55:06 -0300

    Salvando progresso!

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Img from '../components/Img';
 import homeMob1stTitle from '../assets/mobile/home/home_mob_1st_title.svg';
+import homeDesktop1stTitle from '../assets/desktop/home/home_desktop_1st_title.svg';
 import styles from '../styles/pages/home.module.css';
 import StoresBtns from '../components/StoresBtns';
 
@@ -9,9 +10,14 @@ export default function Home() {
     <main className={ styles.homePageMain }>
       <section className={ styles.homepageFirstSection }>
         <Img
-          imgClassName={ styles.homepage1stTitle }
+          imgClassName={ styles.homepage1stTitleMob }
           imgSrc={ homeMob1stTitle }
-          imgAlt="1° título da Home"
+          imgAlt="1° título da Home Mobile"
+        />
+        <Img
+          imgClassName={ styles.homepage1stTitleDesktop }
+          imgSrc={ homeDesktop1stTitle }
+          imgAlt="1° título da Home Desktop"
         />
         <StoresBtns
           containerClassName={ styles.storesBtnsContainer }
