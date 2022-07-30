9cf708ca735da8e7b64c19ce1b8f935ba1fab8e1
João Felipe P <jf.pelliccione@gmail.com>
2022-07-30 13:47:57 -0300

    Salvando progresso!

---
+++ components/home/Homepage1stSection.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Img from '../Img';
+import homeMob1stTitle from '../../assets/mobile/home/home_mob_1st_title.svg';
+import homeDesktop1stTitle from '../../assets/desktop/home/home_desktop_1st_title.svg';
+import qrCode from '../../assets/QR_code.svg';
+import styles from '../../styles/pages/home.module.css';
+import StoresBtns from '../StoresBtns';
+
+export default function Homepage1stSection() {
+  return (
+    <section className={ styles.homepage1stSection }>
+      <Img
+        imgClassName={ styles.home1stSectionTitleMob }
+        imgSrc={ homeMob1stTitle }
+        imgAlt="1° título da Home Mobile"
+      />
+      <Img
+        imgClassName={ styles.home1stSectionTitleDesktop }
+        imgSrc={ homeDesktop1stTitle }
+        imgAlt="1° título da Home Desktop"
+      />
+      <Img
+        imgClassName={ styles.homepageQrCode }
+        imgSrc={ qrCode }
+        imgAlt="QR code para baixar o aplicativo"
+      />
+      <StoresBtns
+        containerClassName={ styles.storesBtnsContainer }
+        btnClassName={ styles.storeBtn }
+      />
+    </section>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,11 @@
 import React from 'react';
-import Img from '../components/Img';
-import homeMob1stTitle from '../assets/mobile/home/home_mob_1st_title.svg';
-import homeDesktop1stTitle from '../assets/desktop/home/home_desktop_1st_title.svg';
-import qrCode from '../assets/QR_code.svg';
 import styles from '../styles/pages/home.module.css';
-import StoresBtns from '../components/StoresBtns';
+import Homepage1stSection from '../components/home/Homepage1stSection';
 
 export default function Home() {
   return (
     <main className={ styles.homePageMain }>
-      <section className={ styles.homepageFirstSection }>
-        <Img
-          imgClassName={ styles.homepage1stTitleMob }
-          imgSrc={ homeMob1stTitle }
-          imgAlt="1° título da Home Mobile"
-        />
-        <Img
-          imgClassName={ styles.homepage1stTitleDesktop }
-          imgSrc={ homeDesktop1stTitle }
-          imgAlt="1° título da Home Desktop"
-        />
-        <Img
-          imgClassName={ styles.homepageQrCode }
-          imgSrc={ qrCode }
-          imgAlt="QR code para baixar o aplicativo"
-        />
-        <StoresBtns
-          containerClassName={ styles.storesBtnsContainer }
-          btnClassName={ styles.storeBtn }
-        />
-      </section>
+      <Homepage1stSection />
       <section className={ styles.homepageSecondSection }>
         <h1>Seção 2</h1>
       </section>
