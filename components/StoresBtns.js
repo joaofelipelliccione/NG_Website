3830a4727bbadd59afe7426dee951a9f2e39fc4b
João Felipe P <jf.pelliccione@gmail.com>
2022-07-30 12:17:50 -0300

    Salvando progresso!

---
+++ components/StoresBtns.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ImgBtn from './ImgBtn';
+import appStore from '../assets/app_store.svg';
+import googlePlayStore from '../assets/google_play_store.svg';
+
+export default function StoresBtns({ containerClassName, btnClassName }) {
+  return (
+    <div className={ containerClassName }>
+      <ImgBtn
+        imgClassName={ btnClassName }
+        imgSrc={ googlePlayStore }
+        imgAlt="Baixe o app na Google Play Store!"
+        href="https://play.google.com/store/apps/details?id=com.neaglebank"
+        passHrefValue
+      />
+      <ImgBtn
+        imgClassName={ btnClassName }
+        imgSrc={ appStore }
+        imgAlt="Baixe o app na Apple App Store!"
+        href="https://apps.apple.com/app/id1480105326"
+        passHrefValue
+      />
+    </div>
+  );
+}
+
+StoresBtns.propTypes = {
+  containerClassName: PropTypes.string.isRequired,
+  btnClassName: PropTypes.string.isRequired,
+};
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Img from '../components/Img';
 import homeMob1stTitle from '../assets/mobile/home/home_mob_1st_title.svg';
 import styles from '../styles/pages/home.module.css';
+import StoresBtns from '../components/StoresBtns';
 
 export default function Home() {
   return (
@@ -11,6 +12,10 @@ export default function Home() {
           imgClassName={ styles.homepage1stTitle }
           imgSrc={ homeMob1stTitle }
           imgAlt="1° título da Home"
+        />
+        <StoresBtns
+          containerClassName={ styles.storesBtnsContainer }
+          btnClassName={ styles.storeBtn }
         />
       </section>
       <section className={ styles.homepageSecondSection }>
