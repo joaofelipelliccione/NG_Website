a158af34857af595810a8debc03a73789e9be705
João Felipe P <jf.pelliccione@gmail.com>
2022-07-30 13:28:25 -0300

    Salvando progresso!

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Img from '../components/Img';
 import homeMob1stTitle from '../assets/mobile/home/home_mob_1st_title.svg';
 import homeDesktop1stTitle from '../assets/desktop/home/home_desktop_1st_title.svg';
+import qrCode from '../assets/QR_code.svg';
 import styles from '../styles/pages/home.module.css';
 import StoresBtns from '../components/StoresBtns';
 
@@ -18,6 +19,11 @@ export default function Home() {
           imgClassName={ styles.homepage1stTitleDesktop }
           imgSrc={ homeDesktop1stTitle }
           imgAlt="1° título da Home Desktop"
+        />
+        <Img
+          imgClassName={ styles.homepageQrCode }
+          imgSrc={ qrCode }
+          imgAlt="QR code para baixar o aplicativo"
         />
         <StoresBtns
           containerClassName={ styles.storesBtnsContainer }
