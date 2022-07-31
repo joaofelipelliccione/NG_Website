a52259a58e779b829f67fd4e92683d1c7e80543c
João Felipe P <jf.pelliccione@gmail.com>
2022-07-31 13:08:57 -0300

    Salvando progresso!

--- components/home/Homepage3rdSection.js
+++ components/home/Homepage3rdSection.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useMediaPredicate } from 'react-media-hook';
 import ImgWithText from '../ImgWithText';
 import ngCardOutOfBox from '../../assets/home_ngcard_out_of_box.svg';
 import descriptionMob from '../../assets/mobile/home/home_mob_3rd_text.svg';
-// import descriptionDesktop from '../../assets/desktop/home/home_desktop_3rd_text.svg';
+import descriptionDesktop from '../../assets/desktop/home/home_desktop_3rd_text.svg';
 import styles from '../../styles/pages/home.module.css';
 
 export default function Homepage3rdSection() {
+  const biggerThan1079 = useMediaPredicate('(min-width: 1079px)');
+
   return (
     <section className={ styles.homepage3rdSection }>
       <ImgWithText
@@ -14,7 +17,7 @@ export default function Homepage3rdSection() {
         img1Src={ ngCardOutOfBox }
         img1Alt="NG.Card saindo da caixa"
         img2ClassName={ styles.homepage3rdSectionTextMob }
-        img2Src={ descriptionMob }
+        img2Src={ biggerThan1079 ? descriptionDesktop : descriptionMob }
         img2Alt="Propósito NG.CASH"
       />
     </section>
