a42432b6e19c3282bafa2f9a954dccf5c99c33bb
João Felipe P <jf.pelliccione@gmail.com>
2022-08-01 12:24:16 -0300

    Salvando progresso!

---
+++ pages/politica-de-privacidade.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../styles/pages/privacyPolicy.module.css';
+
+export default function PrivacyPolicy() {
+  return (
+    <main className={ styles.privacyPolicyPage }>
+      <h1>Olá Privacidade!</h1>
+    </main>
+  );
+}
