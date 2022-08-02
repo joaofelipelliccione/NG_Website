4623197d512f65b80c852eb6502eb82f764216c1
João Felipe P <jf.pelliccione@gmail.com>
2022-08-01 22:28:33 -0300

    Corringindo Hydration!

--- components/home/Homepage4thSection.js
+++ components/home/Homepage4thSection.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { useMediaPredicate } from 'react-media-hook';
 import Img from '../Img';
 import homeMob4thCover from '../../assets/mobile/home/home_mob_4th_cover.png';
 import homeDesktop4thCover from '../../assets/desktop/home/home_desktop_4th_cover.svg';
 import styles from '../../styles/pages/home.module.css';
 
 export default function Homepage4thSection() {
-  const biggerThan1079 = useMediaPredicate('(min-width: 1079px)');
-
   return (
     <section className={ styles.homepage4thSection }>
       <Img
-        imgClassName={ styles.home4thSectionCover }
-        imgSrc={ biggerThan1079 ? homeDesktop4thCover : homeMob4thCover }
+        imgClassName={ styles.homeMob4thSectionCover }
+        imgSrc={ homeMob4thCover }
+        imgAlt="Comece já!"
+      />
+      <Img
+        imgClassName={ styles.homeDesktop4thSectionCover }
+        imgSrc={ homeDesktop4thCover }
         imgAlt="Comece já!"
       />
     </section>
