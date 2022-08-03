d3cf17660337bc1fa47775427dbb593f59315852
João Felipe P <jf.pelliccione@gmail.com>
2022-08-03 12:51:07 -0300

    Salvando progresso!

--- components/home/NewsSection.js
+++ components/home/NewsSection.js
@@ -4,12 +4,14 @@ import newsBillboard from '../../assets/news_billboard.png';
 import styles from '../../styles/components/home/NewsSection.module.css';
 
 const newsIcons = [
-  { iconId: 1, iconImgSrc: '/news/logo_globo.png', iconHref: 'https://oglobo.globo.com/clube-o-globo/servicos/oferta/ngcash-assine-e-ganhe.ghtml' },
-  { iconId: 2, iconImgSrc: '/news/logo_forbes.png', iconHref: 'https://forbes.com.br/forbes-money/2021/06/segmentacao-de-fintechs-conheca-4-bancos-digitais-criados-para-publicos-especificos/#foto4' },
-  { iconId: 3, iconImgSrc: '/news/logo_estadao.png', iconHref: 'https://einvestidor.estadao.com.br/mercado/banco-neaglebank-youtubers-criancas/' },
-  { iconId: 4, iconImgSrc: '/news/logo_folhaSP.png', iconHref: 'https://www1.folha.uol.com.br/mpme/2021/05/empresas-lancam-aplicativos-de-mesada-digital-e-cartao-de-credito-para-criancas.shtml' },
-  { iconId: 5, iconImgSrc: '/news/logo_istoE.png', iconHref: 'https://www.istoedinheiro.com.br/tem-fintech-ate-para-criancas-e-adolescentes/' },
-  { iconId: 6, iconImgSrc: '/news/logo_uol.png', iconHref: 'https://economia.uol.com.br/mais/ultimas-noticias/2021/01/27/fintechs-bancos-digitais-para-criancas-adolescentes.htm' },
+  { iconId: 1, iconImgSrc: '/news/logo_forbes.png', iconHref: 'https://forbes.com.br/forbes-money/2022/08/fintech-ng-cash-recebe-aporte-historico-de-us-10-milhoes/' },
+  // { iconId: 2, iconImgSrc: '/news/logo_bloomberg.png', iconHref: '' },
+  { iconId: 3, iconImgSrc: '/news/logo_valor.png', iconHref: 'https://pipelinevalor.globo.com/startups/noticia/banco-da-new-generation-ngcash-capta-us-10-milhoes-com-a16z-e-monashees.ghtml' },
+  { iconId: 4, iconImgSrc: '/news/logo_globo.png', iconHref: 'https://oglobo.globo.com/blogs/capital/post/2022/08/conta-digital-para-adolescentes-fundada-na-puc-rio-recebe-aporte-de-us-10-milhoes.ghtml' },
+  { iconId: 5, iconImgSrc: '/news/logo_exame.png', iconHref: 'https://exame.com/negocios/ng-cash-app-financeiro-geracao-z-aporte-10-milhoes/' },
+  { iconId: 6, iconImgSrc: '/news/logo_estadao.png', iconHref: 'https://link.estadao.com.br/noticias/inovacao,fintech-ngcash-recebe-us-10-milhoes-para-ser-o-banco-da-geracao-z,70004125001' },
+  // { iconId: 7, iconImgSrc: '/news/logo_infomoney.png', iconHref: '' },
+  { iconId: 8, iconImgSrc: '/news/logo_neofeed.png', iconHref: 'https://neofeed.com.br/blog/home/aposta-da-stone-ng-cash-capta-seed-de-us-10-milhoes-com-a16z-e-monashees/' },
 ];
 
 export default function NewsSection() {
