bf6633b5f2bf720b923d4ea9ce9a5af4bc35aa50
João Felipe P <jf.pelliccione@gmail.com>
2022-08-04 09:46:41 -0300

    Salvando Progresso!

--- components/home/Homepage3rdSection.js
+++ components/home/Homepage3rdSection.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useMediaPredicate } from 'react-media-hook';
+// import { useMediaPredicate } from 'react-media-hook';
 import ImgWithText from '../ImgWithText';
 import EmojisScrollbar from './EmojisScrollbar';
 import ngCardOutOfBox from '../../assets/home_ngcard_out_of_box.svg';
@@ -8,17 +8,26 @@ import descriptionDesktop from '../../assets/desktop/home/home_desktop_3rd_text.
 import styles from '../../styles/pages/home.module.css';
 
 export default function Homepage3rdSection() {
-  const biggerThan1079 = useMediaPredicate('(min-width: 1079px)');
+  // const biggerThan1079 = useMediaPredicate('(min-width: 1079px)');
 
   return (
     <section className={ styles.homepage3rdSection }>
       <ImgWithText
-        wrapperClassName={ styles.homepage3rdSectionWrapper }
+        wrapperClassName={ styles.homepage3rdSectionWrapperMob }
         img1ClassName={ styles.ngCardOutOfBox }
         img1Src={ ngCardOutOfBox }
         img1Alt="NG.Card saindo da caixa"
         img2ClassName={ styles.homepage3rdSectionTextMob }
-        img2Src={ biggerThan1079 ? descriptionDesktop : descriptionMob }
+        img2Src={ descriptionMob }
+        img2Alt="Propósito NG.CASH"
+      />
+      <ImgWithText
+        wrapperClassName={ styles.homepage3rdSectionWrapperDesktop }
+        img1ClassName={ styles.ngCardOutOfBox }
+        img1Src={ ngCardOutOfBox }
+        img1Alt="NG.Card saindo da caixa"
+        img2ClassName={ styles.homepage3rdSectionTextDesktop }
+        img2Src={ descriptionDesktop }
         img2Alt="Propósito NG.CASH"
       />
       <EmojisScrollbar />
